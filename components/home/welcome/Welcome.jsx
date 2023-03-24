4613522a0f9d6c784e9ff5174990c13cfa4e40a3
Ravi Prakash <ravip140891@gmail.com>
2023-03-24 22:10:16 +0530

    Done

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,51 +1,70 @@
-import {useState} from 'react'
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
-import { useRouter } from 'expo-router';
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
-import { icons, SIZES } from '../../../constants';
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
-const jobTypes = ["Full-Time", "Part-Time", "Contract"]
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
-  const [activeJobType, setActiveJobType] = useState("Full-Time")
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
   return (
     <View>
       <View style={styles.container}>
         <Text style={styles.userName}>Hello Ravi</Text>
-        <Text style={styles.welcomeMessage}>Find the perfect job</Text>
+        <Text style={styles.welcomeMessage}>Find perfect job</Text>
       </View>
+
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}
-          value=""
-          onChange={() => {}}
-          placeholder="What are you looking for?"/> 
+          <TextInput
+            style={styles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
+          />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
-          <Image source={icons.search}
-          resizeMode="contain"
-          style={styles.searchBtnImage}/>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
         </TouchableOpacity>
       </View>
+
       <View style={styles.tabsContainer}>
-        <FlatList data={jobTypes}
-        renderItem={({item}) => (
-          <TouchableOpacity style={styles.tab(activeJobType, item)}
-          onPress={() => {
-            setActiveJobType(item);
-            router.push(`/search/${item}`)
-          }}>
-            <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item}
-        contentContainerStyle={{columnGap: SIZES.small}}
-        horizontal/>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "163599821amshd49d2602ac45d3cp1835bcjsn27ebbaf41182",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("Error occurred");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, error, isLoading, refetch };
+};
+
+export default useFetch;
